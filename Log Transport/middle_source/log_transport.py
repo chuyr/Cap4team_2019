2b09e28243ac1d7853eee3cc2028ea9b0f857e4b
chuyr <43734279+chuyr@users.noreply.github.com>
2019-05-08 19:36:26 +0900

    log_transprot
    insert data in database

--- Log Transport/middle_source/log_transport.py
+++ Log Transport/middle_source/log_transport.py
@@ -1,5 +1,8 @@
 import subprocess, os, sys
 import re
+import pymysql as sql
+import threading
+from datetime import datetime
 
 def getMacAddress():    #ipconfig에서 mac주소를 가져오는 함수
     arrinfo = {}
@@ -32,8 +35,13 @@ def getMacAddress():    #ipconfig에서 mac주소를 가져오는 함수
 def tasklist():
     wmic = subprocess.check_output('wmic path win32_process get caption,creationdate', shell=True) #process 시작시간 저장
     wmic = wmic.decode('euc-kr')
+    threading.Timer(600, tasklist).start()
+    print(datetime.now())
 
     return wmic
+
+conn = sql.connect(host='localhost', user='root', passwd='toor', db='test', charset='utf8')       #db 연결
+curs = conn.cursor()        #conn으로부터 cursor 생성
 
 arrinfo = getMacAddress()
 print("Host : " + arrinfo['host']
@@ -41,14 +49,22 @@ print("Host : " + arrinfo['host']
     + "\nMAC 2 : " + arrinfo[1]
     + "\nMAC 3 : " + arrinfo[2])
 
+
 wmic = tasklist()
 
 if re.findall(r'\w+.exe\b', wmic):      #.exe로 끝나는 모든 string을 저장, set() = 중복제거
     process_name = re.findall(r'\w+.exe', wmic)
     start_proc = re.findall(r'2019\w\w\w\w+\w\w\w\w\w\w', wmic)        #2019년도로 시작하는 문자열 검색
 
+sql = """select * from test order by start_proc desc limit 1"""
+last_proc = curs.execute(sql)       #DB에 저장된 데이터 중 가장 최근 시작한 프로세스
+
 for i in range (0, len(process_name)):       #DB에 저장될 컨텐츠
-    print("Index : " , i ,
-        "MAC : " , arrinfo[2] ,
-        "proc_name : " ,process_name[i] ,
-        "start_proc : " ,start_proc[i])
+    if int(start_proc[i]) < int(last_proc):      #start_proc에 저장된 데이터 중 last_proc 보다 작을 경우, 다음 인덱스로 넘어감
+        i+=1
+    sql1 = """insert into test(mac, proc_name, start_proc)
+            values (%s, %s, %s)"""
+    curs.execute(sql1, (arrinfo[2], process_name[i], start_proc[i]))
+    conn.commit()
+
+conn.close()        #conn 닫기
