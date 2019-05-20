20c8cd2236eb1f2da8057c7ea96f51692c05ac4f
chuyr <43734279+chuyr@users.noreply.github.com>
2019-05-20 17:37:02 +0900

    log_transport export db

--- Log Transport/source/log_transport.py
+++ Log Transport/source/log_transport.py
@@ -1,8 +1,8 @@
 import subprocess, os, sys
 import re
-import pymysql as sql
-import threading
-from datetime import datetime
+import pymysql
+import datetime
+import time
 
 def getMacAddress():    #ipconfig에서 mac주소를 가져오는 함수
     arrinfo = {}
@@ -35,36 +35,61 @@ def getMacAddress():    #ipconfig에서 mac주소를 가져오는 함수
 def tasklist():
     wmic = subprocess.check_output('wmic path win32_process get caption,creationdate', shell=True) #process 시작시간 저장
     wmic = wmic.decode('euc-kr')
-    threading.Timer(600, tasklist).start()
-    print(datetime.now())
 
     return wmic
 
-conn = sql.connect(host='localhost', user='root', passwd='toor', db='test', charset='utf8')       #db 연결
+conn = pymysql.connect(host='localhost', user='root', passwd='toor', db='test', charset='utf8')       #db 연결(host='192.168.43.89', user='root', passwd='1234', db='mysql')
 curs = conn.cursor()        #conn으로부터 cursor 생성
 
 arrinfo = getMacAddress()
-print("Host : " + arrinfo['host']
-    + "\nMAC 1 : " + arrinfo[0]
-    + "\nMAC 2 : " + arrinfo[1]
-    + "\nMAC 3 : " + arrinfo[2])
+# print("\nMAC 1 : " + arrinfo[0]
+#     + "\nMAC 2 : " + arrinfo[1]
+#     + "\nMAC 3 : " + arrinfo[2])
+
+now = datetime.datetime.now()       #현재시간
+gap = datetime.timedelta(days=1)
+tomorrow = now + gap     #현재시간부터 1일 후
 
 
-wmic = tasklist()
 
-if re.findall(r'\w+.exe\b', wmic):      #.exe로 끝나는 모든 string을 저장, set() = 중복제거
-    process_name = re.findall(r'\w+.exe', wmic)
-    start_proc = re.findall(r'2019\w\w\w\w+\w\w\w\w\w\w', wmic)        #2019년도로 시작하는 문자열 검색
+for second in range(0, 86400):
+    wmic = tasklist()
 
-sql = """select * from test order by start_proc desc limit 1"""
-last_proc = curs.execute(sql)       #DB에 저장된 데이터 중 가장 최근 시작한 프로세스
+    if re.findall(r'\w+.exe\b', wmic):      #.exe로 끝나는 모든 string을 저장, set() = 중복제거
+        process_name = re.findall(r'\w+.exe', wmic)
+        start_proc = re.findall(r'2019\w\w\w\w+\w\w\w\w\w\w', wmic)        #2019년도로 시작하는 문자열 검색
 
-for i in range (0, len(process_name)):       #DB에 저장될 컨텐츠
-    if int(start_proc[i]) < int(last_proc):      #start_proc에 저장된 데이터 중 last_proc 보다 작을 경우, 다음 인덱스로 넘어감
-        i+=1
-    sql1 = """insert into test(mac, proc_name, start_proc)
-            values (%s, %s, %s)"""
-    curs.execute(sql1, (arrinfo[2], process_name[i], start_proc[i]))
-    conn.commit()
+    curs.execute("select * from test")
+    msg = curs.fetchone()
+    # print(msg)
+    if msg != None:     #테이블에 데이터가 있으면
+        curs.execute("select start_proc from test order by start_proc desc limit 1")       #table name = proc_log, DB에 저장된 데이터 중 가장 최근 시작한 프로세스
+        last_proc = curs.fetchone()
+        print("last_proc : ", int(last_proc[0]))
+        for i in range (0, len(process_name)):       #DB에 저장될 컨텐츠
+            if int(start_proc[i]) < int(last_proc[0]):      #start_proc에 저장된 데이터 중 last_proc 보다 작을 경우, 다음 인덱스로 넘어감, start_proc + index
+                i += 1
+            elif int(start_proc[i]) > int(last_proc[0]):
+                sql = """insert into test(mac, proc_name, start_proc) values (%s, %s, %s)"""  #table name = proc_log
+                curs.execute(sql, (arrinfo[2], process_name[i], start_proc[i]))
+                conn.commit()
+
+    # print(last_proc[0])
+
+    # sql2 = """select id from test order by id desc limit 1"""       #최근 index 값
+    # curs.execute(sql2)
+    # index = curs.fetchone()
+
+    if msg == None:     # 테이블에 데이터가 없을 경우
+        for i in range (0, len(process_name)):       #DB에 저장될 컨텐츠
+            sql1 = """insert into test(mac, proc_name, start_proc) values (%s, %s, %s)"""  #table name = proc_log
+            curs.execute(sql1, (arrinfo[2], process_name[i], start_proc[i]))
+            conn.commit()
+
+    print(datetime.datetime.now())
+    # print(int(last_proc[0]))
+    # print("index : ", index)
+    time.sleep(20)      #30초동안 지연
+    second += 20
 
 conn.close()        #conn 닫기
